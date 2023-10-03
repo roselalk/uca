60138f5b8272b9647bedcd5feb37cc8249b1912c
Rose Lalk <rose.lalk@kvk.nl>
2023-10-03 15:40:33 +0200

    Created basic structure of project

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,14 @@
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <>
+      <header className="hero--container">
+        <h1>UCA</h1>
+        <h2>University Cheerleading Amsterdam</h2>
+      </header>
+    </>
+  );
+}
+
+export default Hero;
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,12 @@
+import "./home.css";
+import Hero from "../hero/Hero";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
+
+export default Home;
